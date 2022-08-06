94979834d4c5be9f0b232a0200f851a4c48616d7
Tab <110726960+Tabiano@users.noreply.github.com>
2022-08-06 22:03:36 +0800

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,11 @@
+# Printing input
+
+name = input ("What is your name?: ")
+age  = input ("what is your age?: ")
+height = input ("What is your height?: ")
+
+print ("Hello "+name)
+print ("your age is "+age)
+print ("your height is "+height+"cm")
+
+# import math
---
+++ Muitiple assignmen.py
@@ -0,0 +1,33 @@
+name = "Ronnie"
+age = 20
+Attractive = True
+
+name, age, Attractive, = "Ronnie", 20, True
+
+print(name)
+print(age)
+print(Attractive)
+
+number1 = 10
+number2 = 10
+number3 = 10
+number4 = 10
+
+number1 = number2 = number3 = number4 = 10
+
+print(number1)
+print(number2)
+print(number3)
+print(number4)
+
+name = "Ronnie"
+print(len(name))
+print(name.find("R"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("n"))
+print(name.replace("R","T"))
+print(name* 5)
